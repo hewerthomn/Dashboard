f576b6501d403db299f2ac21179ba22b1d47270a
Everton Inocencio <hewerthomn@gmail.com>
2015-02-05 15:23:31 -0400

    corrigido caminhos

--- index.php
+++ index.php
@@ -11,37 +11,39 @@ $base = rtrim(str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname(__FILE__)), '/'
 ?><!doctype html>
 <html>
 <head>
-    <title><?php echo $system->os->hostname; ?></title>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" href="<?php echo $base ?>favicon.ico">
-    <meta http-equiv="refresh" content="<?php echo $refresh ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="stylesheet" href="<? echo $base . '/static/gradient.css'; ?>">
+  <title><?php echo $system->os->hostname; ?></title>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" href="<?= $base ?>/favicon.ico">
+  <meta http-equiv="refresh" content="<?php echo $refresh ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="<?= $base ?>/static/gradient.css">
 </head>
+
 <body>
+	<div id="container">
+		<h1>
+		    <?php echo $system->os->hostname; ?>
+		    <small id="uptime">
+		    	online for <?= $system->uptime->days; ?> days,
+		    	<?= $system->uptime->hours; ?> hours,
+		    	<?= $system->uptime->minutes; ?> minutes
+		    </small>
+		</h1>
 
-<div id="container">
+	<?php
+		/* --------------------------------------------------------------
+		 * Show widgets
+		 * -------------------------------------------------------------- */
+		$directory = dirname(__FILE__) . '/widgets';
+		$widgets = scandir($directory,  SCANDIR_SORT_ASCENDING);
 
-<h1>
-    <?php echo $system->os->hostname; ?>
-    <small id="uptime">online for <? echo $system->uptime->days; ?> days, <? echo $system->uptime->hours; ?> hours, <? echo $system->uptime->minutes; ?> minutes </small>
-</h1>
+		foreach ($widgets as $widget) {
+		    if ($widget != '.' && $widget != '..') {
+		        include($directory . '/' . $widget);
+		    }
+		}
+	?>
 
-<?php
-	/* --------------------------------------------------------------
-	 * Show widgets
-	 * -------------------------------------------------------------- */
-	$directory = dirname(__FILE__) . '/widgets';
-	$widgets = scandir($directory,  SCANDIR_SORT_ASCENDING);
-
-	foreach ($widgets as $widget) {
-	    if ($widget != '.' && $widget != '..') {
-	        include($directory . '/' . $widget);
-	    }
-	}
-?>
-
-</div>
-
+	</div>
 </body>
 </html>
